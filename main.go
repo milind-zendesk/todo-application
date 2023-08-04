004980776e19f64b87fc01e8d209cda3092a4ae7
milind-zendesk <milind.shinde@zendesk.com>
2023-08-04 14:19:06 +0530

    init project - Milind

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+//Create todo: POST /todos
+//Retrieve all todos: GET /todos
+//Retrieve todo by id: GET /todos/{id}
+//Update todo by id: PUT /todos/{id}
+//Delete todo by id: DELETE /todos/{id}
