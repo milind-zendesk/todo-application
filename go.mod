cc6d492f4e1f142267a1bbd021a60b9814a6d25e
Devesh Chinchole <devesh.chinchole@zendesk.com>
2023-08-07 11:43:00 +0530

    make file setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module todo-application
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+	RunServer()
+}
+
+func RunServer() {
+	router := mux.NewRouter()
+	router.HandleFunc("/todos", getAllTodos).Methods("GET")
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalln("There's an error with the server ", err)
+	}
+
+}
+
+func getAllTodos(writer http.ResponseWriter, request *http.Request) {
+
+	writer.Header().Set("Content-Type", "application/json")
+	test := map[string]string{"name": "jeffrey"}
+	err := json.NewEncoder(writer).Encode(&test)
+	if err != nil {
+		log.Fatalln("There was an error encoding the initialized struct")
+	}
 }
 
 //Create todo: POST /todos
@@ -13,3 +40,14 @@ func main() {
 //Retrieve todo by id: GET /todos/{id}
 //Update todo by id: PUT /todos/{id}
 //Delete todo by id: DELETE /todos/{id}
+
+// Gorilla Mux
+// sql library + Mysql docker container
+
+// Tasks:
+// design your project structure
+// write unit tests
+// setup http server
+// implement handler
+// (optional) basic UI using the API endpoints
+// Acceptance criteria: should be able to reach each endpoint using curl or postman
