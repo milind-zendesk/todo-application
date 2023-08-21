a57aa8509f208b181f088d1b65f59bc5ba5adac2
Devesh Chinchole <devesh.chinchole@zendesk.com>
2023-08-21 23:29:02 +0530

    todo test cases

--- endpoint/api/todo/post_test.go
+++ endpoint/api/todo/post_test.go
@@ -11,29 +11,31 @@ import (
 	"todo-application/model"
 
 	"github.com/golang/mock/gomock"
-	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_deleteData(t *testing.T) {
+func Test_PostData(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
+	ctx := context.TODO()
 	mockClient := queries.NewMockQueries(ctrl)
 
-	ctx := context.TODO()
-	requestPayload := model.User{
-		Id:       1,
-		Name:     "Milind Shinde",
-		Location: "Pune",
+	requestPayload := model.Todos{
+		Title:    "Work",
+		Status:   "Done",
+		Priority: "High",
+		UserID:   3,
 	}
+
 	body, _ := json.Marshal(requestPayload)
 	request, _ := http.NewRequestWithContext(ctx, http.MethodPost, "/", bytes.NewBuffer(body))
-	request = mux.SetURLVars(request, map[string]string{"id": "1"})
 	responseWriter := httptest.NewRecorder()
+	mockClient.EXPECT().InsertTodoData(requestPayload).Return(nil)
 
-	mockClient.EXPECT().DeleteTodoData(1).Return(nil)
-	DeleteHandler(mockClient)(responseWriter, request)
+	// Call
+	CreateHandler(mockClient)(responseWriter, request)
 
+	// Check
 	resp := responseWriter.Result()
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
--- endpoint/api/todo/put_test.go
+++ endpoint/api/todo/put_test.go
@@ -15,26 +15,26 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_deleteData(t *testing.T) {
+func Test_UpdateData(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
 	mockClient := queries.NewMockQueries(ctrl)
 
 	ctx := context.TODO()
-	requestPayload := model.User{
-		Id:       1,
-		Name:     "Milind Shinde",
-		Location: "Pune",
+	requestPayload := model.Todos{
+		Title:    "Work",
+		Status:   "Done",
+		Priority: "High",
+		UserID:   1,
 	}
 	body, _ := json.Marshal(requestPayload)
 	request, _ := http.NewRequestWithContext(ctx, http.MethodPost, "/", bytes.NewBuffer(body))
 	request = mux.SetURLVars(request, map[string]string{"id": "1"})
 	responseWriter := httptest.NewRecorder()
 
-	mockClient.EXPECT().DeleteTodoData(1).Return(nil)
-	DeleteHandler(mockClient)(responseWriter, request)
+	mockClient.EXPECT().UpdateTodoData(1, requestPayload).Return(nil)
+	UpdateHandler(mockClient)(responseWriter, request)
 
 	resp := responseWriter.Result()
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
 }
